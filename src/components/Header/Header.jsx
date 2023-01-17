f0b8f34c2aeb5eb7c89c3427cc31cf543769ba8c
Edainsv <edainsv.pro@gmail.com>
2023-01-17 01:02:27 +0100

    font-size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 function Header() {
 	return (
 		<header>
-			<div>
+			<Link to="/">
 				<img src={logo} alt="Kasa" />
-			</div>
+			</Link>
 
 			<div>
 				<ul>
